3b61c3b98d28ec7c1b0bc6f835468e5e95b1c5af
Plinio Anel Nunez <nunezplinio@me.com>
2020-01-27 15:28:49 -0500

    Eliminated unnecessary functions.

--- images_to_jpeg.py
+++ images_to_jpeg.py
@@ -1,64 +1,23 @@
 # -*- coding: utf-8 -*-
 import logging
-import os
 
 import click
-from PIL import Image
 from progress.bar import Bar
 
-from handler.file import get_file_name, get_extension, all_files_in
+from handler.file import all_files_in
 from handler.image import is_image, convert_to_jpeg_and_override
-
-
-def resize_with_aspect(im, filename_in, max_size):
-    if max_size == im.size or max_size == (0, 0):
-        # logging.info(f"{filename_in} returning image as has correct size.")
-        return im
-    ratio = min(max_size[0] / im.width, max_size[1] / im.height)
-    new_size = (int(ratio * im.width), int(ratio * im.height))
-    logging.info(f"{filename_in} {im.size} will be changed to {new_size} @ ratio = {ratio}")
-    resized = im.resize(new_size)
-    return resized
-
-
-def convert_to_jpeg(filename_in, filename_out, max_size=(0, 0), overwrite=False):
-    logging.info(f"converting {filename_in} to jpeg")
-    with Image.open(filename_in, mode='r') as im:
-        rgb_im = im.convert('RGB') if im.mode != 'RGB' else im
-        if overwrite:
-            os.remove(filename_in)
-        rgb_im.save(filename_out, "JPEG", optimize=True, quality=95)
-
-
-def get_target_jpeg(original_file, output_dir, extension=".jpg"):
-    original_filename = os.path.basename(original_file)
-    original_extension = get_extension(original_file)
-    new_name = os.path.join(output_dir, original_filename.replace(original_extension, extension))
-    return new_name
-
-
-def get_target_jpeg_with_override(original_file, extension=".jpg"):
-    original_name = get_file_name(original_file)
-    new_name = original_name + ".jpg"
-    return new_name
 
 
 @click.command()
 @click.option("--path", "-i", type=click.Path(), required=True, help="path to open files from")
-@click.option("--output_dir", "-i", type=click.Path(), required=False, help="path to output files to")
-# @click.option('--recursive', '-r', is_flag=True, help="Should it iterate deeper than the first level?")
-@click.option('--max_size', '-s', type=click.Tuple([int, int]), default=(0, 0), help="resizing")
-@click.option('--dry', '-d', is_flag=True, help="Convert the images to jpeg format and save them as 'jpg'.")
-@click.option('--overwrite', '-o', is_flag=True, help="Overwrite current file.")
-def main(path, output_dir, max_size, dry, overwrite):
-    # list all files in folder
+def main(path):
     files = all_files_in(path)
-    # select images
     images = [file for file in files if is_image(file)]
+    logging.info(f"Converting {len(images)} to jpeg")
 
     with Bar('fixing images', max=len(images)) as bar:
         for image in images:
-            convert_to_jpeg_and_override(image)
+            convert_to_jpeg_and_override(image)  # TODO: Paralelizar esto. multiprocessing, joblib, o ray
             bar.next()
 
     logging.info("DONE")
