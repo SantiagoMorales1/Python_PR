5f5c62209d8cbfd8e1b22a8b143874e4f1bb883e
nunezplinio <nunezplinio@me.com>
2020-03-25 19:27:01 -0500

    better paralelism

--- images_to_jpeg.py
+++ images_to_jpeg.py
@@ -3,19 +3,32 @@ import logging
 
 import click
 from p_tqdm import t_map
+import multiprocessing as mp
 
 from handler.file import all_files_in
 from handler.image import is_image, convert_to_jpeg_and_override
 
 
+
+
 @click.command()
 @click.option("--path", "-i", type=click.Path(), required=True, help="path to open files from")
-def main(path):
+@click.option("--processes", "-p", type=int, default=2, required=True, help="nuber of processes to use. -1 all cores, 0 disables paralelism.")
+def main(path, processes):
+    logging.info(f"Looking for images. Please wait a moment.")
     files = all_files_in(path)
     images = [file for file in files if is_image(file)]
     logging.info(f"Converting {len(images)} to jpeg")
 
-    t_map(convert_to_jpeg_and_override, images)
+    if processes == 0:
+        logging.info("Running normally")
+        t_map(convert_to_jpeg_and_override, images)
+    else:
+        processes = mp.cpu_count() if processes == -1 else processes
+        logging.info(f"Runnining using [{processes}] processes")
+        pool = mp.Pool(mp.cpu_count())
+        sults = pool.map(convert_to_jpeg_and_override, images)
+        pool.close()
 
     logging.info("DONE")
 
